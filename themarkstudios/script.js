3429b6036c8bd2a74de4cf63f4375b5ee2197058
Jeffrey Cheung <jcheung@verto.ca>
2013-07-09 17:08:20 -0400

    Updated case studies page with videos and rollovers

--- themarkstudios/script.js
+++ themarkstudios/script.js
@@ -10,15 +10,29 @@ $(document).ready(function() {
   });
 
   // Case Studies Navigation
-  $("#case-studies-nav a").click(function() {
+  $("#case-studies-nav").on('click', 'a', function() {
     $("#case-studies-nav a").each(function() {
+      var image = $(this).find("img").attr("src").substring($(this).find("img").attr("src").lastIndexOf("/"));
       $(this).closest("li").css("background", "none");
+      $(this).closest("li").removeClass("active");
+      $(this).find("img").attr("src", "images/case_studies_nav" + image);
     });
     $("#case-studies-content div.hidden").each(function() {
       $(this).hide();
     });
     $(this).closest("li").css("background-color", "#fff");
+    $(this).closest("li").addClass("active");
+    var image = $(this).find("img").attr("src").substring($(this).find("img").attr("src").lastIndexOf("/"));
+    $(this).find("img").attr("src", "images/case_studies_nav/rollovers" + image);
     $("#case-studies-content").find("div.hidden#" + $(this).attr("id")).show();
   });
   $("#case-studies-nav li").first().find("a").trigger("click");
+  $("#case-studies-nav").on('mouseover', 'li:not(.active)', function() {
+    var image = $(this).find("img").attr("src").substring($(this).find("img").attr("src").lastIndexOf("/"));
+    $(this).find("img").attr("src", "images/case_studies_nav/rollovers" + image);
+  });
+  $("#case-studies-nav").on('mouseout', 'li:not(.active)', function() {
+    var image = $(this).find("img").attr("src").substring($(this).find("img").attr("src").lastIndexOf("/"));
+    $(this).find("img").attr("src", "images/case_studies_nav" + image);
+  });
 });
